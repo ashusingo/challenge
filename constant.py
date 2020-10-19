40185344de8033e0e70fe4d6d1270d2255c7ab6d
ashusingo <42213531+ashusingo@users.noreply.github.com>
2020-10-19 22:08:39 +0530

    Updating DB Name as "userapi"

--- constant.py
+++ constant.py
@@ -6,6 +6,6 @@ HOST=":9001"        # bind to port 9001 on all IPs.
 DB_HOST="localhost" # connect to postgres on this host
 DB_USER="postgres"  # postgres user
 DB_PASS="example"   # postgres password
-DB_NAME="postgres"   # database schema name
+DB_NAME="userapi"   # database schema name
 DB_TABLE="users"
 TOKEN_EXPIRED=10 #Token wil be valid till 10 mins
